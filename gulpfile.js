deda75bd90389afc81f6a92cbbc8d144fc60bf59
Vladimir Galynsky <galynskyy@gmail.com>
2017-01-27 08:23:19 +0000

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,36 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
-var browserSync = require("browser-sync").create();
 var autoprefixer = require("autoprefixer");
 var cssnano = require("cssnano");
 var postcss = require("gulp-postcss");
 
 gulp.task("default", ["build"]);
-gulp.task("dev", ["build", "browserSync", "watch"]);
 gulp.task("build", ["styles", "html", "js", "assets", "fonts"]);
 
 gulp.task("styles", function() {
-	return gulp.src("./src/styles/**/*.css")
+	return gulp.src("./src/components/**/*.css")
 		.pipe(concat("build.min.css"))
 		.pipe(postcss([autoprefixer, cssnano]))
 		.pipe(gulp.dest("./public/styles/"));
 });
 
 gulp.task("html", function() {
-	return gulp.src("./src/*.html")
+	return gulp.src("./src/components/**/*.html")
 		.pipe(gulp.dest("./public/"));
 });
 
 gulp.task("assets", function() {
-    gulp.src("./src/assets/**/*.{svg,jpg,png}")
+    gulp.src("./src/components/**/*.{svg,jpg,png}")
         .pipe(gulp.dest("./public/assets/"));
 });
 
 gulp.task("fonts", function() {
-    gulp.src("./src/fonts/**/*.ttf")
+    gulp.src("./src/components/**/*.ttf")
         .pipe(gulp.dest("./public/fonts/"));
 });
 
 gulp.task("js", function() {
-	return gulp.src("./src/scripts/**/*.js")
+	return gulp.src("./src/components/**/*.js")
 		.pipe(concat("build.js"))
 		.pipe(gulp.dest("./public/scripts/"));
-});
-
-gulp.task("browserSync", function () {
-	browserSync.init({
-    	server: {
-			baseDir: "./public"
-    	}
-	});
-});
-
-gulp.task("watch", function() {
-	gulp.watch("./src/styles/**/*.css", ["styles"]);
-	gulp.watch("./src/*.html", ["html"]);
-	gulp.watch("./src/scripts/**/*.js", ["js"]);
-	gulp.watch('./src/**/*.*', browserSync.reload);
 });
